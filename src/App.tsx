30939165fc315454a5fb5cace24222eeab8c411c
Josh Jack <joshuajack256@gmail.com>
2022-12-24 13:38:11 +0000

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { Navbar } from './components/nav/NavBar'
 import { FooterDesktop } from './components/footer/FooterDesktop'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Homepage from './components'
-import Form from './components/form/Form'
+import Form from '../src/components/form/Form'
+import TagManager, { TagManagerArgs } from 'react-gtm-module'
 
 function App() {
     const location = useLocation()
@@ -13,6 +14,11 @@ function App() {
         window.scrollTo(0, 0)
     }, [location.pathname])
 
+    const gtmId = process.env.REACT_APP_GTM_ID || ''
+    const args: TagManagerArgs = { gtmId }
+    useEffect(() => {
+        TagManager.initialize(args)
+    })
     return (
         <>
             <Navbar>
