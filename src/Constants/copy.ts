a9da36eff1fa4a6076c45bc03a44b3968be7818c
Josh Jack <joshuajack256@gmail.com>
2022-12-24 12:35:42 +0000

    Inital copy will moreless change

---
+++ src/Constants/copy.ts
@@ -0,0 +1,68 @@
+import Intro from '../Assets/Intro.svg'
+import Develop from '../Assets/Develop.svg'
+import Launch from '../Assets/Launch.svg'
+import Blockchain from '../Assets/Blockchain.svg'
+import Tokenomics from '../Assets/Tokenomics.svg'
+import SYT from '../Assets/SYT.svg'
+import MVP from '../Assets/MVP.svg'
+
+export const benefits = [
+    {
+        slug: 'Intro',
+
+        icon: Intro,
+        title: 'Intro',
+        description: 'We will first schedule a call with you to find out what your goals are, and how we can help you attain these goals. From this intro call, we will assemble a team that most closely matches your needs, create a scoping document and begin architecting your project.',
+    },
+    {
+        slug: 'tech',
+
+        icon: Develop,
+        title: 'Develop',
+        description: 'This is where the real magic happens. We will implement the scoping document and architecture of your project. During this process, we will keep you updated every step of the way with frequent updates and a developer standup Cadence that you set.',
+    },
+    {
+        slug: 'coordination',
+
+        icon: Launch,
+        title: 'Launch',
+        description: 'We will make sure that your project is launched without issue. We will remain involved to tackle any issues that may arise, and help you have a successful launch.',
+    },
+]
+
+export const wwd = [
+    {
+      slug: "Intro",
+     
+      icon: Blockchain,
+      title: "Blockchain",
+      description:
+              "We are at the very core, blockchain specialists. We will guide you through all aspects of planning, architecting and executing your blockchain goals."
+      },
+    {
+      slug: "tech",
+     
+      icon: Tokenomics,
+      title: "Tokenomics",
+      description:
+        "We understand that planning a token is difficult. We collectively have years of experience designing successful tokens that perform as YOU want.",
+    },
+    {
+      slug: "coordination",
+   
+      icon: SYT,
+      title: "Grants",
+      description:
+        "Want to utilize a team of bonafide builders to apply for a grant? Partner with us, and we’ll help you get over the finish line.",
+      },
+      {
+          slug: "coordination",
+       
+          icon: MVP,
+          title: "MVP",
+          description:
+            "From ideation, to design and implementation. We will help you go from zero to MVP.",
+        },
+      
+  
+  ];
